724903d93e0a3a5c11be53cb69ac852458697e7e
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-15 18:54:19 +0000

    dongling

---
+++ Proc4/releases/dongle/P4echo.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+?>
+<html>
+<head><title>Player Start Message</title></head>
+<body>
+
+    <p> This is a test interface for the Proc4 system.
+    It will echo the message recieved as json.
+    For more information about Proc4, go to
+        <a href="https://pluto.coe.fsu.edu/Proc4/">Proc 4 home
+    page.</a></p>
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        App: <input type="text" name="app"/><br/>
+        Uid: <input type="text" name="uid"/><br/>
+        Context: <input type="text" name="context"/><br/>
+        Sender: <input type="text" name="sender"/><br/>
+        Message: <input type="text" name="mess"/><br/>
+        Timestamp: <input type="text" name="timestamp"/><br/>
+        data: <input type="text" name="data"/><br/>
+        <input type="submit" name="Postit!"/>
+    </form>
+</body>
+</html>
+
+<?php
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+     $app = $_POST['app'];
+     $ecdapp = strpos($app,'ecd://epls.coe.fsu.edu');
+    if ($ecdapp === false ||  $ecdapp != 0) {
+        die("That application is not supported on this server.");
+    } else {
+      header('Content-Type: application/json;charset=utf-8');
+       echo json_encode($_POST);
+    }
+} else {
+    die("This script only works with GET and POST requests.");
+}
+?>
