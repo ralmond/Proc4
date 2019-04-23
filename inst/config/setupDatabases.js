a68f6c9be0696e9ee1d325e9eb3c7687bebe13ae
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-23 03:05:26 +0000

    databases

--- inst/config/setupDatabases.js
+++ inst/config/setupDatabases.js
@@ -21,12 +21,18 @@ c4User["roles"] = [{role:"readWrite", db:"Proc4"},
                    {role:"read", db:"EIRecords"},
                    {role:"read", db:"ASRecords"},
                    {role:"dbAdmin", db:"Proc4"}];
+db.getSiblingDB("admin").dropUser("C4");
+db.getSiblingDB("admin").createUser(c4User);
+db.getSiblingDB("Proc4").dropUser("C4");
 db.getSiblingDB("Proc4").createUser(c4User);
 
 eiUser = pwds.filter(function(u) {return u.user == "EIP";})[0];
 eiUser["roles"] = [{role:"readWrite", db:"EIRecords"},
                    {role:"read", db:"gameLRS"},
                    {role:"dbAdmin", db:"EIRecords"}];
+db.getSiblingDB("admin").dropUser("EIP");
+db.getSiblingDB("admin").createUser(eiUser);
+db.getSiblingDB("EIRecords").dropUser("EIP");
 db.getSiblingDB("EIRecords").createUser(eiUser);
 
 eaUser = pwds.filter(function(u) {return u.user == "EAP";})[0];
@@ -34,11 +40,17 @@ eaUser["roles"] = [{role:"readWrite", db:"EARecords"},
                    {role:"read", db:"EIRecords"},
                    {role:"read", db:"ASRecords"},
                    {role:"dbAdmin", db:"EARecords"}];
+db.getSiblingDB("admin").dropUser("EAP");
+db.getSiblingDB("admin").createUser(eaUser);
+db.getSiblingDB("EARecords").dropUser("EAP");
 db.getSiblingDB("EARecords").createUser(eaUser);
 
 asUser = pwds.filter(function(u) {return u.user == "ASP";})[0];
 asUser["roles"] = [{role:"readWrite", db:"ASRecords"},
                    {role:"read", db:"EARecords"},
                    {role:"dbAdmin", db:"ASRecords"}];
+db.getSiblingDB("admin").dropUser("ASP");
+db.getSiblingDB("admin").createUser(asUser);
+db.getSiblingDB("ASRecords").dropUser("ASP");
 db.getSiblingDB("ASRecords").createUser(asUser);
 
