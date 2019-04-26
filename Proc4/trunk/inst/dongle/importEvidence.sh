524c75a1993bcec0808c5ea8871ceec9f5f1954c
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-26 18:10:52 +0000

    First pass

--- Proc4/trunk/inst/dongle/importEvidence.sh
+++ Proc4/trunk/inst/dongle/importEvidence.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-query="{timestamp: {\"\$gt\": {\"\$date\": \""
-query+=$1
-query+="\"}}, active:true, voided:false}"
-echo $query
-mongoexport -d gameLRS -c statements -q $query -f statement,timestamp --jsonArray
+mongoimport -d EIRecords -c Events --jsonArray
