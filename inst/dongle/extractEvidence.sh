253d0f07d33dc324d6e849436ccc77e6e175f3aa
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-05-02 17:51:44 +0000

    Feeling Loopy

--- inst/dongle/extractEvidence.sh
+++ inst/dongle/extractEvidence.sh
@@ -3,4 +3,4 @@ query="{timestamp: {\"\$gt\": {\"\$date\": \""
 query+=$1
 query+="\"}}, active:true, voided:false}"
 echo $query
-mongoexport -d gameLRS -c statements -q $query -f statement,timestamp --jsonArray
+mongoexport -d gameLRS -c statements -q "$query" -f statement,timestamp --jsonArray
