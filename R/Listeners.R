8c727420a2dbaf7cc8bacb8a8c6f27e53c79a738
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-07-17 21:42:30 +0000

    Proc4_0.4-4

--- R/Listeners.R
+++ R/Listeners.R
@@ -271,6 +271,94 @@ setMethod("receiveMessage","UpdateListener",
           function(x,mess) x$receiveMessage(mess))
 
 
+##############################################
+## Table Listener -- Based on work by Lukas Liu and Nan Wang
+
+TableListener <-
+  setRefClass("TableListener",
+              fields=list(name = "character",
+                          fieldlist = "character",  #Named list of types
+                          df = "data.frame",
+                          messSet="character"
+                          ),
+              methods=list(
+                  initialize =
+                    function(name="ppData",
+                             fieldlist=c("uid"="character",
+                                         "context"="character"),
+                             messSet=character(),
+                             ...){
+                      callSuper(name=name,
+                                fieldlist=fieldlist,
+                                messSet=messSet,
+                                df=data.frame(),...)
+                },
+                initDF = function(){
+                  ##initializes the data frame with proper dimensions
+                  ##(uid, app, and context and mandatory)
+                  fields <- vector("list",length(fieldlist))
+                  names(fields) <- names(fieldlist)
+                  for (f in names(fields)) {
+                    if (grepl("ordered",fieldlist[f])) {
+                      levels <-  strsplit(sub("ordered\\((.*)\\)","\\1",
+                                              fieldlist[f]),",")[[1]]
+                      fields[[f]] <- ordered(NA,levels)
+                    } else if (grepl("factor",fieldlist[f])) {
+                      levels <- strsplit(sub("factor\\((.*)\\)","\\1",
+                                             fieldlist[f]),",")[[1]]
+                      fields[[f]] <- factor(NA,levels)
+                    } else {
+                      fields[[f]] <- vector(fieldlist[f],1)
+                      fields[[f]][1] <-NA
+                    }
+                  }
+                  df <<- do.call("data.frame",c(fields,stringsAsFactors=FALSE))
+                  flog.trace("First row:",df,capture=TRUE)
+                },
+                receiveMessage = function(mess){
+                  ## captures the incoming message and stick into the
+                  ## data frame.
+                  if (ncol(df)==0L) initDF()
+                  if (mess(mess) %in% messSet) {
+                    flog.debug("Adding row for %s (%s): %s",uid(mess),
+                               context(mess), toString(mess))
+                    new.line <- df[1,,drop=FALSE]
+                    for (f in names(new.line)) {
+                      new.val <- switch(f,
+                                        "app"=app(mess),
+                                        "context"=context(mess),
+                                        "uid"=uid(mess),
+                                        "mess"=mess(mess),
+                                        "sender"=sender(mess),
+                                        "timestamp"=ifelse(typeof(new.line$timestamp) == "character",
+                                                           as.character(timestamp(mess)),
+                                                           timestamp(mess)),
+                                        details(mess)[[f]])
+                      if(!is.null(new.val)) new.line[1,f] <- new.val
+                    }
+                    df <<- rbind(df, new.line)
+                  }
+                },
+                returnDF = function() {
+                  ## deletes the first row of DF because initDF()
+                  ## creates an empty row
+                  df[-1, ]
+                }
+                ))
+
+
+TableListener <- function (name="ppData",
+                             fieldlist=c("uid"="character",
+                                         "context"="character"),
+                             messSet=character(),...) {
+  new("TableListener",name=name,fieldlist=fieldlist,messSet=messSet,...)
+}
+
+setMethod("isListener","TableListener",function(x) TRUE)
+setMethod("receiveMessage","TableListener",
+          function(x,mess) x$receiveMessage(mess))
+
+
 #############################################
 ## Listener Set
 
