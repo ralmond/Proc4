a1e9aa70d1748cc7435cf1aabba726cf746ddf99
Russell G Almond <russell.g.almond@gmail.com>
2023-06-21 11:54:34 -0400

    First draft of schema server code.

---
+++ vignettes/Dongle.R
@@ -0,0 +1,6 @@
+## ----options, include=FALSE---------------------------------------------------
+options(rmarkdown.html_vignette.check_title = FALSE)
+
+## ----P4Lamp, fig.cap="Proc4 Dongle Architecture", out.height="25%", echo=FALSE----
+knitr::include_graphics("Proc4-Lamp.png")
+
